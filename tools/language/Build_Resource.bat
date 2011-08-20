6f0488ef54ac136989be97d24b6f0dcdf8771252
sinojelly <sinojelly@gmail.com>
2011-08-20 22:59:20 +0800

    add init version of language tools.

---
+++ tools/language/Build_Resource.bat
@@ -0,0 +1,68 @@
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\Wiz\Wiz.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\Wiz.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizKMCore\WizKMCore.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizKMCore.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizTools\WizTools.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizTools.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizHtmlEditor\WizHtmlEditor.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizHtmlEditor.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizKMCalendar\WizKMCalendar.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizKMCalendar.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizKMControls\WizKMControls.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizKMControls.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizTodo\WizTodo.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizTodo.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizWebCapture\WizWebCapture.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\NPWizWebCapture.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizWebCaptureResourceToArticle\WizWebCaptureResourceToArticle.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizWebCaptureResourceToArticle.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizKMSendTo\WizKMSendTo.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizKMSendTo.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizViewer\WizViewer.rc" "E:\My Projects\Wiz\Products\Debug\translations\Default\WizViewer.ini"
+
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizWM\WizWM.rc" "E:\My Projects\Wiz\Products\Release_WM\translations\Default\WizWM.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizReaderWM\WizReaderWM.rc" "E:\My Projects\Wiz\Products\Release_Reader_WM\translations\Default\WizReaderWM.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizReader\WizReader.rc" "E:\My Projects\Wiz\WizReader\res\1033.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceExtractor.exe "E:\My Projects\Wiz\WizReaderWM\WizReaderWM.rc" "E:\My Projects\Wiz\WizReaderWM\res\1033.ini"
+
+;1028
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\Wiz.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\Wiz.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMCore.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizKMCore.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizTools.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizTools.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizHtmlEditor.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizHtmlEditor.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMCalendar.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizKMCalendar.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMControls.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizKMControls.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizTodo.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizTodo.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\NPWizWebCapture.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\NPWizWebCapture.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizWebCaptureResourceToArticle.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizWebCaptureResourceToArticle.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMSendTo.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizKMSendTo.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizViewer.ini" "E:\My Projects\Wiz\Products\Debug\translations\1028\WizViewer.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Release_WM\translations\default\WizWM.ini" "E:\My Projects\Wiz\Products\Release_WM\translations\1028\WizWM.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Release_Reader_WM\translations\default\WizReaderWM.ini" "E:\My Projects\Wiz\Products\Release_Reader_WM\translations\1028\WizReaderWM.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\WizReader\res\1033.ini" "E:\My Projects\Wiz\WizReader\res\1028.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\WizReaderWM\res\1033.ini" "E:\My Projects\Wiz\WizReaderWM\res\1028.ini"
+
+
+;2052
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\Wiz.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\Wiz.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMCore.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizKMCore.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizTools.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizTools.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizHtmlEditor.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizHtmlEditor.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMCalendar.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizKMCalendar.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMControls.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizKMControls.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizTodo.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizTodo.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\NPWizWebCapture.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\NPWizWebCapture.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizWebCaptureResourceToArticle.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizWebCaptureResourceToArticle.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizKMSendTo.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizKMSendTo.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Debug\translations\default\WizViewer.ini" "E:\My Projects\Wiz\Products\Debug\translations\2052\WizViewer.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Release_WM\translations\default\WizWM.ini" "E:\My Projects\Wiz\Products\Release_WM\translations\2052\WizWM.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\products\Release_Reader_WM\translations\default\WizReaderWM.ini" "E:\My Projects\Wiz\Products\Release_Reader_WM\translations\2052\WizReaderWM.ini"
+
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\WizReader\res\1033.ini" "E:\My Projects\Wiz\WizReader\res\2052.ini"
+.\..\..\CyberArticlePro\ResourceWorkshop\Products\Debug\ResourceTranslator.exe "E:\My Projects\Wiz\WizReaderWM\res\1033.ini" "E:\My Projects\Wiz\WizReaderWM\res\2052.ini"
